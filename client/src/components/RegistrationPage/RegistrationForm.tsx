caea204f0ca637299b050abb8e5cc24b7d3d77a5
moinur-rahman <rahmanmoin27@gmail.com>
2022-10-11 07:56:38 +0600

    InputBox added in registration form

--- client/src/components/RegistrationPage/RegistrationForm.tsx
+++ client/src/components/RegistrationPage/RegistrationForm.tsx
@@ -1,11 +1,48 @@
-import { Flex, Heading } from "@chakra-ui/react"
+import {
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+import InputBox from "./InputBox";
 
-const RegistrationForm:React.FC = () => {
+const RegistrationForm: React.FC = () => {
   return (
-    <Flex background={"white"} width={"50%"} height={"100%"} > 
-    <Heading color={"#224957"}>Welcome to chat app</Heading>
-    </Flex>
-  )
-}
+    <Center
+      background={"white"}
+      width={"50%"}
+      height={"100%"}
+      flexDirection={"column"}
+    >
+      <Heading color={"#224957"}>Welcome to chat app</Heading>
+      <InputBox
+        labelName={"Email"}
+        inputName={"email"}
+        inputType={"email"}
+        inputPlaceHolder={"Enter your email address"}
+      />
+      <InputBox
+        labelName={"User Name"}
+        inputName={"name"}
+        inputType={"text"}
+        inputPlaceHolder={"Enter your name"}
+      />
+      <InputBox
+        labelName={"Password"}
+        inputName={"password"}
+        inputType={"password"}
+        inputPlaceHolder={"Enter your password"}
+      />
+      <InputBox
+        labelName={"Repeat Password"}
+        inputName={"repeatPassword"}
+        inputType={"password"}
+        inputPlaceHolder={"Enter your password again"}
+      />
+    </Center>
+  );
+};
 
-export default RegistrationForm
+export default RegistrationForm;
