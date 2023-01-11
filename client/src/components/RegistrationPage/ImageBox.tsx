12d94fc5fdb24a69852813a57cc13bca40283649
moinur-rahman <rahmanmoin27@gmail.com>
2023-01-11 16:07:28 +0600

    Update all the package in the server and client and correct the code

--- client/src/components/RegistrationPage/ImageBox.tsx
+++ client/src/components/RegistrationPage/ImageBox.tsx
@@ -3,8 +3,13 @@ import { Box } from "@chakra-ui/react";
 
 const ImageBox: React.FC = () => {
   return (
-    <Box width={"50%"} height={"100%"} position={"relative"} display={{base:"none",form:"inherit"}}>
-      <Image src={"/images/chat.jpg"} alt={"chat-image"} layout={"fill"}/>
+    <Box
+      width={"50%"}
+      height={"100%"}
+      position={"relative"}
+      display={{ base: "none", form: "inherit" }}
+    >
+      <Image src={"/images/chat.jpg"} alt={"chat-image"} fill/>
     </Box>
   );
 };
